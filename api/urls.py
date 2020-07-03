32cb36500394ce82a7f41d6300c9108cf12c72c8
carrie <2153074275@qq.com>
2020-07-03 08:51:29 +0800

    部分修改

--- api/urls.py
+++ api/urls.py
@@ -4,5 +4,7 @@ from api import views
 
 urlpatterns = [
     path('user/',views.UserAPIView.as_view()),
-    path('test/',views.TestPessionAPIView.as_view())
+    path('test/',views.TestPessionAPIView.as_view()),
+    path('msg/',views.SendMsgAPIView.as_view()),
+    path('login/',views.UserLoginOrReadOnly.as_view()),
 ]
